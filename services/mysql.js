5cbcc126105ee2588c251412bdaf0d96dbac4b60
priyanka-tute <120934183+priyanka-tute@users.noreply.github.com>
2022-12-18 18:47:27 +0530

    Add files via upload

--- services/mysql.js
+++ services/mysql.js
@@ -3,13 +3,44 @@
 const mysql = require('mysql2');
 const Course = require('../models/course');
 const Student = require("../models/student");
-const connection = mysql.createConnection({
-  host: 'tutedude.com',
+// const connection = mysql.createConnection({
+//   host: 'tutedude.com',
+//   user: 'tutedudeuser',
+//   password: 'Tutedude@694',
+//   database: 'tutedude_payment'
+// });
+
+var db_config = {
+    host: 'tutedude.com',
   user: 'tutedudeuser',
   password: 'Tutedude@694',
   database: 'tutedude_payment'
-});
-
+  };
+  
+  var connection;
+  
+  function handleDisconnect() {
+    connection = mysql.createConnection(db_config); // Recreate the connection, since
+                                                    // the old one cannot be reused.
+  
+    connection.connect(function(err) {              // The server is either down
+      if(err) {                                     // or restarting (takes a while sometimes).
+        console.log('error when connecting to db:', err);
+        setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+      }                                     // to avoid a hot loop, and to allow our node script to
+    });                                     // process asynchronous requests in the meantime.
+                                            // If you're also serving http, display a 503 error.
+    connection.on('error', function(err) {
+      console.log('db error', err);
+      if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+        handleDisconnect();                         // lost due to either server restart, or a
+      } else {                                      // connnection idle timeout (the wait_timeout
+        throw err;                                  // server variable configures this)
+      }
+    });
+  }
+  
+  handleDisconnect();
 
 
 getOrAddStudentFromMysql = (email) => {
